33f696487c9dcf7a33b10a769d462863d0514558
Santiago Vasquez Franco <santiagop200101@gmail.com>
2024-02-06 00:19:02 +0100

    avances

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,7 +2,20 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hola),
+    path('home/', views.index),
     path('about/', views.about),
-    path('hello/<str:user>', views.hola),
+    path('hello/<str:user>', views.hello),
+
+    #se pueden ingresar variables para que sean vistas en el url del servidor, 
+    #se especifica el tipo de dato, que en este caso es str y la variable que en 
+    #este caso es username, se puede llenar con cualquier nombre la variable 
+    #Ejemplo: 
+
+    path('project/', views.project),
+
+    #Santiago/ o Diego/, pero no se puede dejar vacio, o presentara un error 404, porque este espera algun tipo de parametro
+
+    path('task/<str:title>', views.task),
+
+
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,26 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+#se importa la libreria models, que tiene los objetos
+from .models import Proyecto, Task
+from django.shortcuts import get_list_or_404
 # Create your views here.
 
 def index(request):
     return HttpResponse("index page")
-def hola(request, user):
-    print(type(user))
-    return HttpResponse('<h1>Hello %s</h1>' % user)
+
+def hello(request, username):
+    # para obtener el parametro impreso en el body
+    # se usa HttpResponse('<h2> Hello %s </h2>' % username)
+    return HttpResponse('<h2> Hello %s </h2>' % username)
 
 def about(request):
     return HttpResponse('About')
+
+def project(request):
+    #   Crea una lista de los objetos, y la guarda en un Json
+    project = list(Proyecto.objects.values())
+    return JsonResponse(project, safe=False)
+
+def task(request, title):
+    #   Obtener datos de un objeto mediante 
+    task = Task.objects.get(title=title)    
+    return HttpResponse('task: %s' % task.title)
